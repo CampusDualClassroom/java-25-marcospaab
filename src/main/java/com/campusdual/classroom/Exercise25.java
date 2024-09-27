539b518fe8beac30cf39c1243ee96e34e9839e00
marcos.padin@outlook.es <you@example.com>
2024-09-27 13:50:52 +0200

    Tarea 25 finalizada

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -1,20 +1,30 @@
 package com.campusdual.classroom;
 
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
-
+        Stack<String> s = new Stack<>();
+        s.add("Smith");
+        s.add("Montessori");
+        s.add("Peralta");
+        s.add("House");
+        return s;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
+        while (!stack.isEmpty()){
+            System.out.println(stack.peek());
 
+            stack.pop();
+        }
     }
 
     public static void main(String[] args) {
+        Stack<String> s = createStack();
+        printAndEmptyStack(s);
 
     }
-
-
 
 }
